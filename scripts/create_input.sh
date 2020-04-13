ec0e54de46a2e957a3b0f842e4f09f8ef4419d78
nuthanc <nuthanc@juniper.net>
2020-04-13 11:54:42 -0700

    Update create_input

--- scripts/create_input.sh
+++ scripts/create_input.sh
@@ -2,7 +2,7 @@ n20_ip_in_file=`grep -o "CONFIG_API_VIP: .*" contrail_test_input.yaml | awk '{pr
 juju_n20_ip=`juju status|grep noden20|awk '{print $3}'`
 kstone_ip_in_file=`grep -o "KEYSTONE_AUTH_HOST: .*" contrail_test_input.yaml | awk '{print $2}'`
 juju_kstone_ip=`juju status|grep keystone/|awk '{print $5}'`
-n19_ip_in_file=`grep noden19 contrail_test_input.yaml -A 1|grep "ip: .*" -o|awk '{print $2'}`
+n19_ip_in_file=`grep noden19 contrail_test_input.yaml -A 1|grep "ip: .*" -o|awk '{print $2}'`
 juju_n19_ip=`juju status|grep noden19|awk '{print $3}'`
 
 # Replace all n20,n19,keystone ips
