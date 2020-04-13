a7e9aa478981075718d13794d0c8ff73ebdbc917
nuthanc <nuthanc@juniper.net>
2020-04-13 11:58:14 -0700

    Parse contrail_test_input.yaml once for sed

--- scripts/create_input.sh
+++ scripts/create_input.sh
@@ -6,6 +6,6 @@ n19_ip_in_file=`grep noden19 contrail_test_input.yaml -A 1|grep "ip: .*" -o|awk 
 juju_n19_ip=`juju status|grep noden19|awk '{print $3}'`
 
 # Replace all n20,n19,keystone ips
-sed -i "s/${n20_ip_in_file}/${juju_n20_ip}/g" contrail_test_input.yaml
-sed -i "s/${n19_ip_in_file}/${juju_n19_ip}/g" contrail_test_input.yaml
-sed -i "s/${kstone_ip_in_file}/${juju_kstone_ip}/g" contrail_test_input.yaml
+sed -i -e "s/${n20_ip_in_file}/${juju_n20_ip}/g" \
+-e "s/${n19_ip_in_file}/${juju_n19_ip}/g" \
+-e "s/${kstone_ip_in_file}/${juju_kstone_ip}/g" contrail_test_input.yaml
