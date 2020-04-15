62402be18b5ec08c80f98976eb88dcfa0633bb95
nuthanc <nuthanc@juniper.net>
2020-04-15 02:11:50 -0700

    Update cmd

--- upgrade-ziu/cmd.sh
+++ upgrade-ziu/cmd.sh
@@ -1,6 +1,9 @@
 juju run-action contrail-controller/leader upgrade-ziu
-juju config contrail-agent image-tag=master-latest
-juju config contrail-agent kernel-hugepages-2m=2
+juju config contrail-agent kernel-hugepages-2m=1024
+juju config contrail-agent image-tag=master.1171
 juju run-action contrail-agent/0 upgrade
-# After 5 minutes
-sudo reboot
+
+ juju config contrail-analytics image-tag=master.1171 
+ juju config contrail-analyticsdb image-tag=master.1171
+ juju config contrail-openstack image-tag=master.1171
+ juju config contrail-controller image-tag=master.1171
