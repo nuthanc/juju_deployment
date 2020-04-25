602d1a15a744fba2fc31d9239c8e34393e9e637c
nuthanc <nuthanc@juniper.net>
2020-04-25 04:00:28 -0700

    Update ziu cmd

--- upgrade-ziu/cmd.sh
+++ upgrade-ziu/cmd.sh
@@ -1,12 +1,3 @@
-juju run-action contrail-controller/leader upgrade-ziu
-juju config contrail-agent image-tag=master.1169 kernel-hugepages-2m=1024
-
-juju config contrail-analytics image-tag=master.1169 
-juju config contrail-analyticsdb image-tag=master.1169
-juju config contrail-openstack image-tag=master.1169
-juju config contrail-controller image-tag=master.1169
-
-juju run-action contrail-agent/0 upgrade
-
-# According to Alexey only the below command is sufficient
-juju config contrail-agent image-tag=master.1169 kernel-hugepages-2m=1024
+juju upgrade-charm --force-units contrail-agent --path ./tf-charms/contrail-agent
+juju config contrail-agent image-tag=master.1188 kernel-hugepages-2m=1024
+juju run-action contrail-agent/0 --wait upgrade
