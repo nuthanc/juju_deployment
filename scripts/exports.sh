d7ee9231176f3c1ce0ee9e7d4a4db85113c2fcf5
nuthanc <nuthanc@juniper.net>
2020-06-09 03:35:52 -0700

    Everything except agilio is working bundle file

--- scripts/exports.sh
+++ scripts/exports.sh
@@ -6,10 +6,10 @@ export n19=$juju_n19_ip
 
 export n20_local="127.0.1.1 noden20.maas noden20"
 export n19_local="127.0.1.1 noden19.maas noden19"
-# export n20_dns="${n20} noden20.maas noden20"
-# export n19_dns="${n19} noden19.maas noden19"
-export n20_dns="192.168.40.20 noden20.maas noden20"
-export n19_dns="192.168.40.19 noden19.maas noden19"
+export n20_dns="${n20} noden20.maas noden20"
+export n19_dns="${n19} noden19.maas noden19"
+# export n20_dns="192.168.40.20 noden20.maas noden20"
+# export n19_dns="192.168.40.19 noden19.maas noden19"
 
 echo "ssh ubuntu@${n20} \"sudo sed -i -e '/${n20_local}/i ${n20_dns}' -e '/${n20_local}/i ${n19_dns}' /etc/hosts\"" > 20remote.sh
 echo "ssh ubuntu@${n19} \"sudo sed -i -e '/${n19_local}/i ${n19_dns}' -e '/${n19_local}/i ${n20_dns}' /etc/hosts\"" > 19remote.sh
