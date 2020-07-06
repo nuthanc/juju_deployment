f1890df26589d38dd4ef043e45637b9eca033b86
nuthanc <nuthanc@juniper.net>
2020-07-06 02:12:42 -0700

    Update exports scripts to support both mgmt and cntrl of /etc/hosts

--- scripts/exports.sh
+++ scripts/exports.sh
@@ -1,3 +1,6 @@
+ssh-keygen -f "/root/.ssh/known_hosts" -R "192.168.30.20"
+ssh-keygen -f "/root/.ssh/known_hosts" -R "192.168.30.19"
+
 juju_n20_ip=`juju status|grep noden20|awk '{print $3}'`
 juju_n19_ip=`juju status|grep noden19|awk '{print $3}'`
 
@@ -8,13 +11,15 @@ export n20_local="127.0.1.1 noden20.maas noden20"
 export n19_local="127.0.1.1 noden19.maas noden19"
 export n20_dns="${n20} noden20.maas noden20"
 export n19_dns="${n19} noden19.maas noden19"
-# export n20_dns="192.168.40.20 noden20.maas noden20"
-# export n19_dns="192.168.40.19 noden19.maas noden19"
+export n20_dns_ctl="192.168.40.20 noden20.maas noden20"
+export n19_dns_ctl="192.168.40.19 noden19.maas noden19"
 
 echo "ssh ubuntu@${n20} \"sudo sed -i -e '/${n20_local}/i ${n20_dns}' -e '/${n20_local}/i ${n19_dns}' /etc/hosts\"" > 20remote.sh
+echo "ssh ubuntu@${n20} \"sudo sed -i -e '/${n20_local}/i ${n20_dns_ctl}' -e '/${n20_local}/i ${n19_dns_ctl}' /etc/hosts\"" >> 20remote.sh
 echo "ssh ubuntu@${n19} \"sudo sed -i -e '/${n19_local}/i ${n19_dns}' -e '/${n19_local}/i ${n20_dns}' /etc/hosts\"" > 19remote.sh
+echo "ssh ubuntu@${n19} \"sudo sed -i -e '/${n19_local}/i ${n19_dns_ctl}' -e '/${n19_local}/i ${n20_dns_ctl}' /etc/hosts\"" >> 19remote.sh
 
 bash 20remote.sh
 bash 19remote.sh
 
-# Create alias for this like alias e="source /root/juju_deployment/scripts/exports.sh"
+# Create alias for this like alias e="source /root/juju_deployment/scripts/exports.sh"  
