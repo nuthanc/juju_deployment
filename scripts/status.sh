98025b25b814fa7368bce55f103aac6a97973faa
nuthanc <nuthanc@juniper.net>
2020-07-28 23:54:56 -0700

    Update status script

--- scripts/status.sh
+++ scripts/status.sh
@@ -7,5 +7,8 @@ while [[ ${n20_status} != *"Deployed"* ]] || [[ ${n29_status} != *"Deployed"* ]]
 	n20_status=$(juju status|grep noden20)
 	n29_status=$(juju status|grep noden29)
 	i34_status=$(juju status|grep nodei34)
+	echo $n20_status
+	echo $n29_status
+	echo $i34_status
 	sleep 5
 done
