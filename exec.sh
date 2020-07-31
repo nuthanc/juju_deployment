3681000de9ff7d9404b503407d706b572ae1e874
nuthanc <nuthanc@juniper.net>
2020-07-31 01:53:10 -0700

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,3 +1,3 @@
-juju deploy ./bundle_multi_ha.yml && bash scripts/status.sh && e
+juju deploy ./bundle_multi_ha.yml && bash scripts/status.sh && python3 /root/juju_deployment/scripts/etc-hosts.py
 
 # Usage: nohup bash exec.sh &
